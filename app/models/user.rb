c0fcad56f5d820ea2a0a889781e99dad7dd1549d
lorenzoash <z.lakyo@gmail.com>
2018-04-19 10:42:08 -0700

    Add dependent destroy

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
     validates :email, presence: true, uniqueness: true
     has_attached_file :image, styles: { medium: "500x500>", thumb: "250x250>" }, presence: true
     validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
-    has_many :orders
+    has_many :orders, dependent: :destroy
     
     def cart
     end
