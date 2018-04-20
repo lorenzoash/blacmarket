4ab1fc07457b36a8f2cdb0128e88cee38335648c
lorenzoash <z.lakyo@gmail.com>
2018-04-20 08:44:53 -0700

    fix user login

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-    before_action :authorize_admin
+    before_action :authorize_admin, except: [:show, :index, :like]
     
     def index
         @items = Item.all
