06de141d47d1bde80d572b43646a86e15fe6bf93
lorenzoash <z.lakyo@gmail.com>
2018-04-19 19:22:55 -0700

    updated bootstrap styling

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -6,22 +6,27 @@ class ChargesController < ApplicationController
     
     def create
         # Amount in cents
-        @amount = 500
+        # @amount = 50000
+        order = current_user.cart
+        order.paid = true
+        order.save
+
+        redirect_to '/charges'
+  
+        # customer = Stripe::Customer.create(
+        #   :email => params[:stripeEmail],
+        #   :source  => params[:stripeToken]
+        # )
       
-        customer = Stripe::Customer.create(
-          :email => params[:stripeEmail],
-          :source  => params[:stripeToken]
-        )
+        # charge = Stripe::Charge.create(
+        #   :customer    => customer.id,
+        #   :amount      => @amount,
+        #   :description => 'BlacMarket Purchase',
+        #   :currency    => 'usd'
+        # )
       
-        charge = Stripe::Charge.create(
-          :customer    => customer.id,
-          :amount      => @amount,
-          :description => 'Jordan Retro 3s',
-          :currency    => 'usd'
-        )
-      
-      rescue Stripe::CardError => e
-        flash[:error] = e.message
-        redirect_to root_path  
+      # rescue Stripe::CardError => e
+      #   flash[:error] = e.message
+      #   redirect_to root_path  
       end
 end
